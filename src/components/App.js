58fd2f8e42c237211a0507629ab683395cb973c6
Keval Khatri <kevalkhatri32@gmail.com>
2023-02-03 00:54:16 +0530

    Add logout functionality and Private Routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,25 @@ import AuthProvider from "../context/AuthContext";
 import Signup from "./Signup";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
+import ForgotPassword from "./ForgotPassword";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route exact path="/" element={<Dashboard />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          ></Route>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
